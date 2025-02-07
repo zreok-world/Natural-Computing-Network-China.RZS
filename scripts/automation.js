2ca8cf13269eae8ee68e693730093e77d2f5b004
徐飞鸿 <186348290+AirVin9526@users.noreply.github.com>
2025-02-07 23:27:40 +0800

    Update automation.js
    Signed-off-by: 徐飞鸿 <186348290+AirVin9526@users.noreply.github.com>

--- scripts/automation.js
+++ scripts/automation.js
@@ -4,3 +4,21 @@ function automate() {
 }
 
 automate();
+
+const fs = require('fs');
+
+function cleanUpOldLogs() {
+    const logsDir = './logs';
+    fs.readdir(logsDir, (err, files) => {
+        if (err) return console.error('无法读取日志目录', err);
+        
+        files.forEach(file => {
+            if (file.endsWith('.log')) {
+                fs.unlinkSync(`${logsDir}/${file}`);
+                console.log(`已删除日志文件: ${file}`);
+            }
+        });
+    });
+}
+
+cleanUpOldLogs();
